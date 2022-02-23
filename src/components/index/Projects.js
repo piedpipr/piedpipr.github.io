c79eb652ddc3e579056c256af5442e14c9fbc12c
piedpipr <43669876+piedpipr@users.noreply.github.com>
2022-02-23 17:40:42 +0600

    small tweaks

--- src/components/index/Projects.js
+++ src/components/index/Projects.js
@@ -1,7 +1,9 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import styled, { keyframes } from "styled-components";
 import Modal from "react-bootstrap/Modal";
 import { Button } from "react-bootstrap";
+import { FaEllipsisH } from "@react-icons/all-files/fa/FaEllipsisH";
+import { FaCloudsmith } from "@react-icons/all-files/fa/FaCloudsmith";
 import "../../styles/projects.css";
 const SectionHeader = styled.div`
   background-color: #5454696a;
@@ -23,29 +25,47 @@ export default function Projects() {
     p4: false,
     p5: false,
     p6: false,
+    status: false,
   });
-
   const changeHandler = (e) => {
     let newState = { ...state, [e]: true };
     setState(newState);
   };
 
-  const Hide = () => {
-    setState({
+  const Hide = (e) => {
+    let newState = {
       p1: false,
       p2: false,
       p3: false,
       p4: false,
       p5: false,
       p6: false,
-    });
+      status: true,
+    };
+    setState(newState);
   };
+  // useEffect(() => {
+  //   if (state.status === true) {
+  //     let newState = {
+  //       p1: false,
+  //       p2: false,
+  //       p3: false,
+  //       p4: false,
+  //       p5: false,
+  //       p6: false,
+  //       status: false,
+  //     };
+  //     setState(newState);
+  //     window.location = `#myProjects`;
+  //   }
+  // });
   const projects = [
     {
       key: "p1",
       name: "GradMate - Android App",
       description: "This is a project",
       type: "App",
+      tech: "react-native, android, firebase, javascript",
       git: "https://github.com/piedpipr/gradmate-app",
       deployment: "#",
       md: "https://raw.githubusercontent.com/piedpipr/gradmate-app/main/README.md",
@@ -55,6 +75,7 @@ export default function Projects() {
       name: "Selenium Chromedriver Automation: Autotrader",
       description: "This is a project",
       type: "Software",
+      tech: "python, selenium, chromedriver",
       git: "https://github.com/piedpipr/cg-trade-automation",
       deployment: "",
       md: "https://raw.githubusercontent.com/piedpipr/cg-trade-automation/main/README.md",
@@ -64,6 +85,7 @@ export default function Projects() {
       name: "Gatsby React Portfolio Site",
       description: "This is a project",
       type: "Web",
+      tech: "gatsbyjs, reactjs, javascript",
       git: "https://github.com/piedpipr/piedpipr.github.io",
       deployment: "https://protik.me",
       md: "https://raw.githubusercontent.com/piedpipr/piedpipr.github.io/master/README.md",
@@ -73,6 +95,7 @@ export default function Projects() {
       name: "Django Site UI",
       description: "This is a project",
       type: "Web",
+      tech: "django, python",
       git: "https://github.com/piedpipr/RECursionNITD-website",
       deployment: "https://recursionnitd.in/",
       md: "",
@@ -82,6 +105,7 @@ export default function Projects() {
       name: "Smart Leave Management System",
       description: "This is a project",
       type: "Web",
+      tech: "nodejs, mongodb, expressjs, hanlebars, passporjs",
       git: "https://github.com/piedpipr/bcc-smart-leave-system/",
       deployment: "",
       md: "https://raw.githubusercontent.com/piedpipr/bcc-smart-leave-system/master/README.md",
@@ -91,6 +115,7 @@ export default function Projects() {
       name: "Kindle Ebook Thumbnail Fix",
       description: "This is a project",
       type: "Web",
+      tech: "python, qt4",
       git: "https://github.com/piedpipr/kindle-thumbnail-fix",
       deployment: "",
       md: "https://raw.githubusercontent.com/piedpipr/kindle-thumbnail-fix/main/README.md",
@@ -102,8 +127,12 @@ export default function Projects() {
     return (
       <div>
         {/* Modal Start */}
-        <Modal show={state[key]} onHide={Hide}>
-          <Modal.Header closeButton></Modal.Header>
+        <Modal show={state[key]} onHide={(event) => Hide()}>
+          <Modal.Header closeButton>
+            <span className="text-dark">
+              It may take a moment to load, refresh if needed
+            </span>
+          </Modal.Header>
           <Modal.Body>
             <iframe
               title="projects"
@@ -113,13 +142,19 @@ export default function Projects() {
             />
           </Modal.Body>
           <Modal.Footer>
-            <Button variant="primary" onClick={Hide}>
+            <Button
+              variant="primary"
+              onClick={(event) => window.open(project.git, "_blank")}
+            >
               Git Repo
             </Button>
-            <Button variant="primary" onClick={Hide}>
+            <Button
+              variant="primary"
+              onClick={(event) => window.open(project.deployment, "_blank")}
+            >
               See Deployment
             </Button>
-            <Button variant="secondary" onClick={Hide}>
+            <Button variant="secondary" onClick={(event) => Hide()}>
               Close
             </Button>
           </Modal.Footer>
@@ -130,26 +165,32 @@ export default function Projects() {
             <div className="project-column">
               <div className="task">
                 <div className="task__tags">
-                  <span className="task__tag task__tag--App">
-                    {project.type}
-                  </span>
+                  {project.type == "App" ? (
+                    <span className="task__tag task__tag--App">
+                      {project.type}
+                    </span>
+                  ) : null}
+                  {project.type == "Web" ? (
+                    <span className="task__tag task__tag--Web">
+                      {project.type}
+                    </span>
+                  ) : null}
+                  {project.type == "Software" ? (
+                    <span className="task__tag task__tag--Softwares">
+                      {project.type}
+                    </span>
+                  ) : null}
+
                   <button className="task__options">
-                    <i className="fas fa-ellipsis-h"></i>
+                    <FaEllipsisH />
                   </button>
                 </div>
                 <p>{project.name}</p>
                 <div className="task__stats">
                   <span>
-                    <time datetime="2021-11-24T20:00:00">
-                      <i className="fas fa-flag"></i>Nov 24
-                    </time>
+                    <FaCloudsmith />
                   </span>
-                  <span>
-                    <i className="fas fa-comment"></i>3
-                  </span>
-                  <span>
-                    <i className="fas fa-paperclip"></i>7
-                  </span>
+                  <span>{project.tech}</span>
                   <span className="task__owner"></span>
                 </div>
               </div>
